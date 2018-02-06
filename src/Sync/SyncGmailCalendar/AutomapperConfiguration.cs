a33a1e45eb5ada445cb706ac766662c899c970a5
Pawel Wujczyk <pwujczyk@hotmail.com>
2018-02-06 16:23:36 +0100

    proper date automapper fix (custom resolver)

--- src/Sync/SyncGmailCalendar/AutomapperConfiguration.cs
+++ src/Sync/SyncGmailCalendar/AutomapperConfiguration.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Google.Apis.Calendar.v3.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,15 +15,30 @@ namespace SyncGmailCalendar
 
         public static void Configure()
         {
-            dtoConfig=new MapperConfiguration(cfg =>
+            dtoConfig = new MapperConfiguration(cfg =>
+              {
+                  cfg.CreateMap<Google.Apis.Calendar.v3.Data.Event, PSCalendarContract.Dto.Event>()
+                  .ForMember(dst => dst.Name, opt => opt.MapFrom(src => src.Summary))
+                  .ForMember(dst => dst.Date, opt => opt.ResolveUsing<DateResolver, EventDateTime>(x => x.Start))// .MapFrom(src => src.Start.DateTime))
+                  .ForMember(dst => dst.EventGuid, opt => opt.Ignore())
+                  .ForMember(dst => dst.NiceId, opt => opt.Ignore())
+                  .ForMember(dst => dst.Type, opt => opt.Ignore());
+              });
+        }
+    }
+
+    public class DateResolver : IMemberValueResolver<object, object, EventDateTime, DateTime>
+    {
+        public DateTime Resolve(object source, object destination, EventDateTime sourceMember, DateTime destMember, ResolutionContext context)
+        {
+            if (sourceMember.DateTime.HasValue)
             {
-                cfg.CreateMap<Google.Apis.Calendar.v3.Data.Event, PSCalendarContract.Dto.Event>()
-                .ForMember(dst => dst.Name, opt => opt.MapFrom(src => src.Summary))
-                .ForMember(dst => dst.Date, opt => opt.MapFrom(src => src.Start.DateTime))
-                .ForMember(dst => dst.EventGuid, opt => opt.Ignore())
-                .ForMember(dst => dst.NiceId, opt => opt.Ignore())
-                .ForMember(dst => dst.Type, opt => opt.Ignore());
-            });
+                return sourceMember.DateTime.Value;
+            }
+            else
+            {
+                return DateTime.Parse(sourceMember.Date);
+            }
         }
     }
 }
